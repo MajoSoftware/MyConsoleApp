294d8f09db2771f348cf06792037c5b81f6a2d77
MajoSoftware <majosoftware@gmail.com>
2016-12-29 13:33:39 +0100

    local change of program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world - changed remote...");
-            Console.WriteLine("Second line");
+            Console.WriteLine("Hello world2...");
+
         }
     }
 }
